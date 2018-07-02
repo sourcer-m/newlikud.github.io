eb6dbe3127c3486934850b8f5af112db9983d016
hertzel.zion <hertzel.zion@yahoo.com>
2018-07-03 01:08:28 +0300

    Added signature pads

---
+++ web-form.js
@@ -0,0 +1,49 @@
+function getWebFormId(fieldName) {
+  return "web_form_" + fieldName;
+}
+
+function initializePads() {
+  var webFormCanvases = document.getElementsByClassName('web-form-canvas');
+  Array.from(webFormCanvases).forEach((c) => {
+    var signaturePad = new Pad(c);
+    signaturePad.init();
+  })
+}
+
+function buildInputField(f) {
+  var fId = getWebFormId(f.name);
+  var input = null;
+  if (f.type === "input") {
+    input = '<input type="text" class="form-control" id="' + fId + '" aria-describedby="emailHelp" placeholder="">';
+      //<small id="emailHelp" class="form-text text-muted">הערה לגבי השדה הנ״ל</small>
+  } else if (f.type === "signature") {
+    input =  `<canvas class="field-form web-form-canvas" id="` + fId + `" width="` + f.width + `" height="` + f.size + `"></canvas>`;
+  }
+  return `<div class="form-group">
+        <label for="` + fId + `">` + f.name + `</label>
+        ` + input + `
+      </div>`;
+}
+
+function buildWebForm() {
+  var html = '';
+  fields.forEach((field) => {
+    html += buildInputField(field);
+  });
+  html += '<button type="button" class="btn btn-default navbar-btn btn-primary" id="save-button">שלח</button>';
+  document.getElementById("web-form").innerHTML = html;
+
+  initializePads();
+}
+
+function fillCanvasForm() {
+  fields.forEach((f) => {
+    var wfID = getWebFormId(f.name);
+    console.log(wfID, f.name)
+    if (f.type === "input") {
+      document.getElementById(f.name).value = document.getElementById(wfID).value;
+    } else if (f.type === "signature") {
+      document.getElementById(f.name).getContext('2d').drawImage(document.getElementById(wfID), 0, 0);
+    }
+  });
+}
