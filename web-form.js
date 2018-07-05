abd5b14fd11a0d1ce119bd9b577ade69077398d6
hertzel.zion <hertzel.zion@yahoo.com>
2018-07-06 02:07:57 +0300

    Added clear buttons

--- web-form.js
+++ web-form.js
@@ -3,29 +3,34 @@ function getWebFormId(fieldName) {
 }
 
 function initializePads() {
-  var webFormCanvases = document.getElementsByClassName('web-form-canvas');
+  let webFormCanvases = document.getElementsByClassName('web-form-canvas');
   Array.from(webFormCanvases).forEach((c) => {
     var signaturePad = new SignaturePad(c, {
         minWidth: .01,
         maxWidth: .8,
         penColor: penColor
     });
-    // signaturePad.init();
-  })
+    let clearBtn = document.querySelector("input[data-target='" + c.id + "']");
+    clearBtn.pad = signaturePad;
+    clearBtn.onclick = () => {
+      clearBtn.pad.clear();
+    }
+  });
 }
 
 function buildInputField(f) {
-  var fId = getWebFormId(f.name);
-  var input = null;
+  let fId = getWebFormId(f.name);
+  let input = null;
   if (f.type === "input") {
     input = '<input type="text" class="form-control" id="' + fId + '" aria-describedby="emailHelp" placeholder="">';
       //<small id="emailHelp" class="form-text text-muted">הערה לגבי השדה הנ״ל</small>
   } else if (f.type === "signature") {
     let style = "border:1px dashed #cccccc; background: url('register-to-likud.png'); background-size: 1140px 2052.34px; background-position: -" + f.x + "px -" + f.y + "px";
     input =  `<canvas class="field-form web-form-canvas" id="` + fId + `" width="` + f.width + `" height="` + f.size + `" style="` + style + `"></canvas>`;
+    input += '<input type="button" class="btn btn-warning btn-sm" value="X" data-target="' + fId + '"/>';
   }
   return `<div class="form-group row">
-        <div class="col-xs-8">
+        <div class="col-xs-8 align-middle">
           ` + input + `
         </div>
         <label for="` + fId + `" class="col-xs-3 col-form-label">` + f.heb + `</label>
@@ -33,7 +38,7 @@ function buildInputField(f) {
 }
 
 function buildWebForm() {
-  var html = '';
+  let html = '';
   fields.forEach((field) => {
     html += buildInputField(field);
   });
@@ -45,7 +50,7 @@ function buildWebForm() {
 
 function fillCanvasForm() {
   fields.forEach((f) => {
-    var wfID = getWebFormId(f.name);
+    let wfID = getWebFormId(f.name);
     console.log(wfID, f.name)
     if (f.type === "input") {
       document.getElementById(f.name).value = document.getElementById(wfID).value;
