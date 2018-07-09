e0e729030d88c77dec8cef8c5b354f3fbc117c7d
hertzel.zion <hertzel.zion@yahoo.com>
2018-07-09 22:12:01 +0300

    Refactored new form-validation.js file and mark complimentary fields

---
+++ form-validation.js
@@ -0,0 +1,55 @@
+function markAsValid(element, valid) {
+  let rootElement = element.parentElement.parentElement
+  if (valid) {
+    rootElement.classList.remove("has-error");
+  } else {
+    rootElement.classList.add("has-error");
+  }
+  Array.from(rootElement.getElementsByClassName("help-block")).forEach((e) => {
+    e.style.display = valid?'none':'block';
+  });
+}
+
+function validateForm() {
+  let invalidElement = null;
+
+  fields.forEach((f) => {
+    if (f.autoField || f.allowEmpty)
+      return;
+
+    if (f.doubleFormOnly && document.getElementsByName('double-form-radio')[0].checked) {
+      return;
+    }
+    
+    let wfID = getWebFormId(f.name);
+    let element = document.getElementById(wfID);
+    if (isEmpty(f, element)) {
+      if (!invalidElement) {
+        invalidElement = element;
+      }
+      markAsValid(element, false);
+    }
+  });
+
+  if (invalidElement) {
+    cursorFocus(invalidElement);
+  }
+
+  return (!invalidElement);
+}
+
+function initializeValidation() {
+  fields.forEach((f) => {
+    if (f.autoField || f.allowEmpty)
+      return;
+    
+    let wfID = getWebFormId(f.name);
+    let element = document.getElementById(wfID);
+
+    element.oninput = function() {
+      markAsValid(element, !isEmpty(f, element));
+    };
+    element.onpropertychange = element.oninput;
+    element.onblur = element.oninput;
+  });
+}
